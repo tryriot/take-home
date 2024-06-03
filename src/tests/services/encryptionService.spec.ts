dbbf56d4228794f77828be265fbffa93a605b7b6
Kevin DANG <kevin.dang01@proton.me>
2024-06-03 16:59:43 +0200

    refac(encryption): clean encryption services

---
+++ src/tests/services/encryptionService.spec.ts
@@ -0,0 +1,85 @@
+import { expect } from 'chai';
+import { EncryptionService } from '../../services/encryptionService';
+import {Base64Algorithm, EncryptionAlgorithm} from "../../services/encryptionAlgorithm";
+
+describe('EncryptionService', () => {
+    let encryptionService: EncryptionService;
+
+    before(() => {
+        encryptionService = new EncryptionService(new Base64Algorithm());
+    });
+
+    describe('encryptObject', () => {
+        it('should encrypt every value in the object', () => {
+            const obj = {
+                foo: 'bar',
+                baz: 123,
+                nested: { nestedProp: 'nestedValue' }
+            };
+
+            const encryptedObj = encryptionService.encryptObject(obj);
+
+            Object.keys(obj).forEach(key => {
+                expect(encryptedObj).to.have.property(key);
+                expect(encryptedObj[key]).to.be.a('string');
+            });
+        });
+
+        it('should return an empty object if the input object is empty', () => {
+            const obj = {};
+            const encryptedObj = encryptionService.encryptObject(obj);
+
+            expect(Object.keys(encryptedObj)).to.have.lengthOf(0);
+        });
+    });
+
+    describe('decryptObject', () => {
+        it('should decrypt every value in the object', () => {
+            const obj = {
+                foo: 'YmFy', // 'bar' encrypted
+                baz: 'MTIz', // '123' encrypted
+                nested: 'eyJuZXN0ZWRQcm9wIjoibmVzdGVkVmFsdWUifQ==' // '{"nestedProp":"nestedValue"}' encrypted
+            };
+
+            const decryptedObj = encryptionService.decryptObject(obj);
+
+            expect(decryptedObj.foo).to.equal('bar');
+            expect(decryptedObj.baz).to.equal(123);
+            expect(decryptedObj.nested).to.deep.equal({ nestedProp: 'nestedValue' });
+        });
+
+        it('should return an empty object if the input object is empty', () => {
+            const obj = {};
+            const decryptedObj = encryptionService.decryptObject(obj);
+
+            expect(Object.keys(decryptedObj)).to.have.lengthOf(0);
+        });
+    });
+
+    describe('setAlgorithm', () => {
+        class MockAlgorithm implements EncryptionAlgorithm {
+            encrypt(data: string): string {
+                return `mock-encrypt(${data})`;
+            }
+
+            decrypt(data: string): string {
+                return data.replace('mock-encrypt(', '').replace(')', '');
+            }
+        }
+
+        it('should set a new encryption algorithm', () => {
+            const mockAlgorithm = new MockAlgorithm();
+
+            // Change l'algorithme d'encryption
+            encryptionService.setAlgorithm(mockAlgorithm);
+
+            // Teste que l'algorithme a bien été changé en chiffrant un objet
+            const obj = { foo: 'bar' };
+            const encryptedObj = encryptionService.encryptObject(obj);
+            const decryptedObj = encryptionService.decryptObject(encryptedObj)
+
+            expect(encryptedObj.foo).to.equal('mock-encrypt("bar")');
+            expect(decryptedObj).to.be.deep.equal({ foo: 'bar' });
+        });
+    })
+});
