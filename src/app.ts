a083205159aaf5da40a69a60564f4c059061b366
Kevin DANG <kevin.dang01@proton.me>
2024-06-03 12:07:51 +0200

    feat(*): write implem for decryption API

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { encryptRouter } from './routes/encrypt';
+import { decryptRouter } from './routes/decrypt';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/encrypt', encryptRouter);
+app.use('/decrypt', decryptRouter);
 
 export default app;
--- src/services/encryptionService.ts
+++ src/services/encryptionService.ts
@@ -9,3 +9,17 @@ export const encryptObject = (obj: any): any => {
   }
   return encryptedObj;
 };
+
+export const decryptObject = (obj: any): any => {
+  const decryptedObj: any = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      try {
+        decryptedObj[key] = JSON.parse(Buffer.from(obj[key], 'base64').toString('utf-8'));
+      } catch (e) {
+        decryptedObj[key] = Buffer.from(obj[key], 'base64').toString('utf-8');
+      }
+    }
+  }
+  return decryptedObj;
+};
